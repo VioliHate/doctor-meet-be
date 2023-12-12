1ce288c6a918521d49bf90c6f379aee730eb9c7d
Violihate <dome.violi90@gmail.com>
2023-12-12 11:22:34 +0100

    Initial commit

---
+++ src/main/java/com/example/doctormeetbe/DoctorMeetBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctormeetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorMeetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorMeetBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doctormeetbe/DoctorMeetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctormeetbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorMeetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
